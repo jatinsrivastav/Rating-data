05a47f5a1050521deff2e5492c9222fd08347877
jatin <jatinsrivastav.mrj@gmail.com>
2023-07-30 14:26:29 +0530

    first commit

---
+++ src/test/java/com/Ratingdata/RatingDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ratingdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
